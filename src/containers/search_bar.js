7c8f445cd46bad7b804ddb3c4930befe2bc6c81c
Matthew Bridges <mattbridgesbusiness@gmail.com>
2017-10-11 21:18:17 +0100

    flesh out form with on submit/input handlers to update component state

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''};
+
+         // Binds the context of the callback function to the component scope.
+        this.onInputChange = this.onInputChange.bind(this);
+    
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+    
     render() {
         return (
-            <form className='input-group'>
-                <input />
-                <span className='input-group-button'>
+            <form onSubmit={this.onFormSubmit} className='input-group'>
+                <input 
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                    className='form-control'
+                    placeholder='Get a five-day forecast in your favourite cities'    
+                />
+                <span className='input-group-btn'>
                     <button className='btn btn-secondary' type='submit'>
                         Search
                     </button>
