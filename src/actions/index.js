22a927c571b116e48c5b004eacb2bd60c4922b22
Matthew Bridges <mattbridgesbusiness@gmail.com>
2017-10-21 14:59:49 +0100

    Add Fetch Weather request. Add Axios and redux-promise to project base. Create fetch weather action creator that calls the openweathermap API.

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const OPEN_WEATHER_MAP_API_KEY = '01015017983529627a02c26e8706924a';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${OPEN_WEATHER_MAP_API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},uk`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+
+    }
+}
